d9f9971ae167c4cd97595c293f320968f6f84c40
TheAwesome98 <TheAwesome98@programmer.net>
2023-03-27 17:32:27 +0100

    code that definitely works -w-

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![feature(const_option_ext)]
+
+use rust_socketio::{ClientBuilder, Payload, RawClient};
+
+const VERSION: &str = option_env!("CARGO_PKG_VERSION").unwrap_or("(unknown)");
+
+fn main() {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    log::info!("starting img v{}", VERSION);
+    log::info!("connecting to server...");
+    ClientBuilder::new("http://wasteof.money")
+        .on("updateMessageCount", |payload: Payload, _socket: RawClient| {
+            match payload {
+                Payload::String(string) => log::info!("Received: {}", string),
+                data => log::warn!("can't handle data: {:#?}", data),
+            }
+        })
+        .on("error", |err, _| log::error!("Error: {:#?}", err))
+        .connect()
+        .expect("failed to connect to the server");
+}
