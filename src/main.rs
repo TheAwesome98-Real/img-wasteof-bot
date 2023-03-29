62f15ddad79ae469ae2628f9302da7365438fe9b
TheAwesome98 <TheAwesome98@programmer.net>
2023-03-29 17:48:51 +0100

    get token

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,79 @@
 #![feature(const_option_ext)]
 
 use rust_socketio::{asynchronous::ClientBuilder, Payload};
+use serde::Deserialize;
 
 const VERSION: &str = option_env!("CARGO_PKG_VERSION").unwrap_or("(unknown)");
 const SERVER: &str = "https://api.wasteof.money";
+
+mod cfg {
+    use serde::Deserialize;
+
+    #[derive(Deserialize)]
+    pub struct Authentication {
+        pub username: String,
+        pub password: String,
+    }
+
+    #[derive(Deserialize)]
+    pub struct Configuration {
+        pub authentication: Authentication,
+    }
+}
+
+#[derive(Deserialize)]
+struct Token {
+    token: String,
+}
 
 #[tokio::main]
 async fn main() {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     log::info!("starting img v{}", VERSION);
+    log::info!("loading configuration...");
+    let config_string = match std::fs::read_to_string("Img.toml") {
+        Ok(string) => string,
+        Err(err) => {
+            log::error!("could not load configuration: {err}");
+            std::process::exit(1);
+        }
+    };
+    let config: cfg::Configuration = match toml::from_str(&config_string) {
+        Ok(cfg) => cfg,
+        Err(err) => {
+            log::error!("could not parse configuration: {err}");
+            std::process::exit(1);
+        }
+    };
+    log::info!("authenticating...");
+    let body = serde_json::json!({
+        "username": config.authentication.username,
+        "password": config.authentication.password
+    });
+    let token = match reqwest::Client::default()
+        .post(format!("{SERVER}/session"))
+        .json(&body)
+        .send()
+        .await
+    {
+        Ok(res) => match res.text().await {
+            Ok(text) => match serde_json::from_str::<Token>(&text) {
+                Ok(token) => token.token,
+                Err(err) => {
+                    log::error!("could not parse token: {err}");
+                    std::process::exit(1);
+                },
+            },
+            Err(err) => {
+                log::error!("could not parse text: {err}");
+                std::process::exit(1);
+            }
+        },
+        Err(err) => {
+            log::error!("could not post /session: {err}");
+            std::process::exit(1);
+        }
+    };
     log::info!("connecting to server '{}'...", SERVER);
     match ClientBuilder::new(SERVER)
         .on("updateMessageCount", |payload, _client| match payload {
@@ -43,4 +108,8 @@ async fn main() {
             log::error!("raw error is as follows:\n {err:#?}")
         }
     }
+    /*loop {
+        log::info!("updating bio with current time");
+        // "@lily's wasteof.money image editor bot (last ping at {})"
+    }*/
 }
