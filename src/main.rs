e3a77d8c937aa69b41285871e357bfd30a49a913
TheAwesome98 <TheAwesome98@programmer.net>
2023-03-29 16:57:23 +0100

    async'ify everything

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,41 @@
 #![feature(const_option_ext)]
 
-use rust_socketio::{ClientBuilder, Payload, RawClient};
+use rust_socketio::{asynchronous::ClientBuilder, Payload};
 
 const VERSION: &str = option_env!("CARGO_PKG_VERSION").unwrap_or("(unknown)");
 const SERVER: &str = "https://api.wasteof.money";
 
-fn main() {
+#[tokio::main]
+async fn main() {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     log::info!("starting img v{}", VERSION);
     log::info!("connecting to server '{}'...", SERVER);
     match ClientBuilder::new(SERVER)
-        .on(
-            "updateMessageCount",
-            |payload: Payload, _socket: RawClient| match payload {
-                Payload::String(string) => log::info!("Received: {}", string),
-                data => log::warn!("can't handle data: {:#?}", data),
-            },
-        )
-        .on("error", |err, _| log::error!("Error: {:#?}", err))
+        .on("updateMessageCount", |payload, _client| match payload {
+            Payload::String(string) => Box::pin(async move {
+                log::info!("Received: {}", string);
+            }),
+            data => Box::pin(async move {
+                log::warn!("can't handle data: {:#?}", data);
+            }),
+        })
+        .on("error", |err, _client| {
+            Box::pin(async move {
+                log::error!("Error: {:#?}", err);
+            })
+        })
+        .on("close", |_payload, _client| {
+            Box::pin(async move {
+                log::info!("disconnected from server");
+            })
+        })
+        .on("message", |payload, _| {
+            Box::pin(async move {
+                log::info!("other message: {payload:#?}");
+            })
+        })
         .connect()
+        .await
     {
         Ok(_client) => log::info!("connected!"),
         Err(err) => {
