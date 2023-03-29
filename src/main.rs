6f39749821d4ec8000069bebe38221ee0700916b
TheAwesome98 <TheAwesome98@programmer.net>
2023-03-29 15:46:19 +0100

    i'm so fucking stupid >w<

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,27 @@
 use rust_socketio::{ClientBuilder, Payload, RawClient};
 
 const VERSION: &str = option_env!("CARGO_PKG_VERSION").unwrap_or("(unknown)");
+const SERVER: &str = "https://api.wasteof.money";
 
 fn main() {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     log::info!("starting img v{}", VERSION);
-    log::info!("connecting to server...");
-    match ClientBuilder::new("http://wasteof.money")
-        .on("updateMessageCount", |payload: Payload, _socket: RawClient| {
-            match payload {
+    log::info!("connecting to server '{}'...", SERVER);
+    match ClientBuilder::new(SERVER)
+        .on(
+            "updateMessageCount",
+            |payload: Payload, _socket: RawClient| match payload {
                 Payload::String(string) => log::info!("Received: {}", string),
                 data => log::warn!("can't handle data: {:#?}", data),
-            }
-        })
+            },
+        )
         .on("error", |err, _| log::error!("Error: {:#?}", err))
-        .connect() {
-            Ok(_client) => log::info!("connected!"),
-            Err(err) => log::error!("failed to connect to the server: {err}: {err:?}"),
+        .connect()
+    {
+        Ok(_client) => log::info!("connected!"),
+        Err(err) => {
+            log::error!("failed to connect to the server: {err}");
+            log::error!("raw error is as follows:\n {err:#?}")
         }
+    }
 }
